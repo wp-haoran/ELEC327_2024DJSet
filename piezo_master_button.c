ae8d6c18696c91c82b172a4a0c34d478319f11d9
Ether3alInfinity <32858116+Ether3alInfinity@users.noreply.github.com>
2024-04-26 14:34:31 -0500

    pushed new test versions

---
+++ piezo_master_button.c
@@ -0,0 +1,162 @@
+#include <msp430.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+char printdist[] = "Distance to the nearest object is: ";
+char centimeter[] = " cm";
+char dot[] = ".";
+char zerro[] = "0";
+char newline[] = " \r\n";
+volatile int temp[2];
+volatile float diff;
+volatile unsigned int i=0;
+int dst_int;
+int dst_flt;
+float tmp_flt;
+char dst_char[5];
+char dst_flt_char[5];
+volatile float distance;
+void ser_output(char *str);
+
+volatile char received_ch = 0;
+int which_octave = 0;
+
+void main(void)
+
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    BCSCTL1= CALBC1_1MHZ;
+    DCOCTL = CALDCO_1MHZ;
+
+
+
+    //pins spi
+    P1OUT |= BIT5;
+    P1DIR |= BIT5 | BIT6;
+    P1SEL = BIT1 | BIT2 | BIT4 | BIT6;
+    P1SEL2 = BIT1 | BIT2 | BIT4;
+
+
+    //pins ultrasonic//
+       //P1DIR = BIT6;
+        P2SEL = BIT1;
+        //P1SEL = BIT1|BIT2|BIT6;
+        //P1SEL2 = BIT1|BIT2;
+        //
+
+
+            P1DIR &= ~BIT3;
+            P1IE |= BIT3; // interrupt enable
+            P1IES |= BIT3; // setting edge select
+            P1IFG &= ~BIT3; // set flag to 0
+
+    //spi
+    UCA0CTL1 = UCSWRST;
+    UCA0CTL0 |= UCCKPH + UCMSB + UCMST + UCSYNC; // 3-pin, 8-bit SPI master
+    UCA0CTL1 |= UCSSEL_2; // SMCLK
+    UCA0BR0 |= 0x02; // /2
+    UCA0BR1 = 0; //
+    UCA0MCTL = 0; // No modulation
+    UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+    //
+
+    //uart
+//    UCA0CTL1 |= UCSWRST+UCSSEL_2;
+//    UCA0BR0 = 52;
+//    UCA0BR1 = 0;
+//    UCA0MCTL = UCBRS_0;
+//    UCA0CTL1 &= ~UCSWRST;
+    //
+
+    //ultrasonic
+    TA0CTL = TASSEL_2|MC_1 ;
+    TA0CCR0 = 0xFFFF;
+    TA0CCR1 = 0x000A;
+    TA0CCTL1 = OUTMOD_7;
+    TA1CTL = TASSEL_2|MC_2 ;
+    TA1CCTL1 = CAP | CCIE | CCIS_0 | CM_3 | SCS ;
+    //
+
+    _enable_interrupts();
+
+    while(1){
+        distance = diff/58;
+        dst_int = floor(distance);
+
+        P1OUT &= (~BIT5); // Select Device
+
+        while (!(IFG2 & UCA0TXIFG)); // USCI_A0 TX buffer ready?
+
+        if (dst_int < 15){
+            UCA0TXBUF = 0xA1;
+        } else if ((dst_int > 15) && (dst_int < 30)) {
+            UCA0TXBUF = 0xA2;
+        } else if ((dst_int > 30) && (dst_int < 45)) {
+            UCA0TXBUF = 0xA3;
+        } else if ((dst_int > 45) && (dst_int < 60)) {
+            UCA0TXBUF = 0xA4;
+        } else
+
+
+
+            if ((dst_int > 60) && (dst_int < 75)) {
+            UCA0TXBUF = 0xA5;
+        } else if ((dst_int > 75) && (dst_int < 90)) {
+            UCA0TXBUF = 0xA6;
+        } else if ((dst_int > 90) && (dst_int < 105)) {
+            UCA0TXBUF = 0xA7;
+        } else {
+            UCA0TXBUF = 0xA8;
+        }
+        if (which_octave)
+            UCA0TXBUF += 0x10;
+         // Send 0xAA over SPI to Slave
+        while (!(IFG2 & UCA0RXIFG)); // USCI_A0 RX Received?
+        received_ch = UCA0RXBUF; // Store received data
+
+        P1OUT |= (BIT5); // Unselect Device
+
+
+
+        __delay_cycles(100000);  //0.5 second delay
+
+
+    }
+}
+#pragma vector = TIMER1_A1_VECTOR
+__interrupt void Timer_A(void){
+        temp[i] = TA1CCR1;
+        i += 1;
+        TA1CCTL1 &= ~CCIFG ;
+        if (i==2) {
+            diff=temp[i-1]-temp[i-2];
+            i=0;
+        }
+
+}
+
+// On board button ISR to play different octaves
+# pragma vector=PORT1_VECTOR
+__interrupt void PORT1_ISR(void)
+{
+    // Green LED on every press
+//    P1DIR ^= BIT0;
+//    P1DIR &= ~BIT3;
+    if (which_octave) {
+        // which_octave is now 3rd octave
+        which_octave = 0;
+//        P1IES ^= BIT3;
+//        P1IFG &= ~BIT3;
+    }
+    else {
+        // which_octave is now 4th octave
+        which_octave = 1;
+//        P1IES ^= BIT3;
+//        P1IFG &= ~BIT3;
+    }
+    // default case to use Port 1.3 for interrupt processing
+//    P1IES ^= BIT3;
+    P1IFG &= ~BIT3;
+}
