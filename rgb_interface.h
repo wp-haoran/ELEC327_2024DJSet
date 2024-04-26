4020855c9fe797047739a049b2a3c3936a91e66a
wp-haoran <111918556+wp-haoran@users.noreply.github.com>
2024-04-26 10:34:25 -0500

    Update rgb_interface.h
    Adjusted documentation and input parameters in line with previous commit. Protected private methods with 'static'.

--- rgb_interface.h
+++ rgb_interface.h
@@ -43,7 +43,7 @@ void rgb_init_spi(void);
  * @param brightness Brightness value of the color
  * @return Void.
  */
-void change_color(uint8_t *color, int color_id, uint8_t brightness); // TODO: make static after testing
+static void change_color(uint8_t *color, int color_id, uint8_t brightness);
 
 /**
  * @brief Sends color to serial buffer for sending to each LED.
@@ -56,20 +56,20 @@ void change_color(uint8_t *color, int color_id, uint8_t brightness); // TODO: ma
  * @param wait_for_completion Boolean indicating whether color is last to be sent.
  * @return Void.
  */
-void rgb_send_color(const uint8_t *color, int light_on, bool wait_for_completion); // TODO: make static after testing
+static void rgb_send_color(const uint8_t *color, char light_on, bool wait_for_completion);
 
 /**
  * @brief Processes row of binary values to be displayed on LED matrix.
  *
  * Helper function for rgb_send_frame.
  *
- * @param row An array of 16 binary values indicating whether LED should be on.
+ * @param row An array of 2 bytes with each bit indicating whether LED should be on.
  * @param color An array of uint8_t values in GRB format specifying color.
  * @param reversed Integer dictating whether row should be read in reverse.
  * @param last_row Boolean indicating whether row is last of LED matrix.
  * @return Void.
  */
-void rgb_send_row(const int *row, uint8_t *color, int reversed, bool last_row); // TODO: make static after testing
+static void rgb_send_row(const char *row, uint8_t *color, bool reversed, bool last_row);
 
 /**
  * @brief Processes frame of binary values to be displayed on LED matrix.
@@ -79,11 +79,11 @@ void rgb_send_row(const int *row, uint8_t *color, int reversed, bool last_row); 
  * 1-yellow, 2-green, 3-cyan, 4-blue, 5-magenta, 6-white. Any other
  * value passed to color_id turns the LED off.
  *
- * @param frame A 16x16 array of binary values illustrating the image to be displayed.
+ * @param frame A 16x2 array of bytes whose bits illustrate the image to be displayed.
  * @param color An array of length 3 allocated for storing the color value.
  * @param color_id An integer 0-6 (inclusive) indicating the color.
  * @return Void.
  */
-void rgb_send_frame(const int *frame, uint8_t *color, int color_id, uint8_t brightness);
+void rgb_send_frame(const char (*frame)[2], uint8_t *color, int color_id, uint8_t brightness);
 
 #endif /* RGB_INTERFACE_H_ */
